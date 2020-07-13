9defc463e83765c7534ebe890c370ad301eab8ac
Joel Danowitz <joel@danowitz.com>
2020-07-13 01:09:00 -0500

    add in ref for round method

--- src/movingAvg.cpp
+++ src/movingAvg.cpp
@@ -2,7 +2,7 @@
 // https://github.com/JChristensen/movingAvg
 // Copyright (C) 2018 by Jack Christensen and licensed under
 // GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html
-
+#include <Arduino.h>
 #include <movingAvg.h>
 
 // initialize - allocate the interval array
